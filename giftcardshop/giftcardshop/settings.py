a7d3151f94c49c176ffec7fecfb6380f798ac996
Paweł Sawicki <psawicki5@gmail.com>
2020-06-09 21:14:54 +0200

    add docker compose

--- giftcardshop/giftcardshop/settings.py
+++ giftcardshop/giftcardshop/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'giftcardshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME", default = "giftcards"),
+        'USER': os.getenv("DB_USER", default = "postgres"),
+        'PASSWORD': os.getenv("DB_PASSWORD", default = "postgres"),
+        'HOST': os.getenv("DB_HOST", default = "db"),
+        'PORT': os.getenv("DB_PORT", default = 5432),
     }
 }
 
